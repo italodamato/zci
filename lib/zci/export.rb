461676b1e35dadb61b2914f15740464312e560ed
italodamato <45588475+italodamato@users.noreply.github.com>
2018-12-04 12:55:46 -0800

    Update export.rb

--- lib/zci/export.rb
+++ lib/zci/export.rb
@@ -36,7 +36,6 @@ module ZCI
     article = {
       id: article_xml[:id],
       section_id: article_xml[:section_id],
-      draft: true,
       title: article_xml.xpath('title').text,
       body: article_xml.xpath('body').text,
       position: article_xml[:position],
